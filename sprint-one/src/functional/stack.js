e1db88aeb381ace76554afe2185efd5ecd74aeea
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-02-05 12:35:57 -0800

    Functional Stack - Mostly Complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,23 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  var key = 0;
+
   someInstance.push = function(value) {
+    storage[key] = value;
+    key++
   };
 
   someInstance.pop = function() {
+    delete storage[key];
+    key--;
   };
 
   someInstance.size = function() {
+    if (key <= 0) {
+      return 0;
+    }
+      return key;
   };
 
   return someInstance;
