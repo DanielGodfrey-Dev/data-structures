d6e30ee9e381940cb2b4a7a13b46128291c06256
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-02-05 20:25:16 -0800

    Prototypal - Queue implemented

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.key = 0;
+  someInstance.deq = 0;
+  someInstance.objectSize = 0;
+
+  return someInstance;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+	enqueue: function(value) {
+		var string = value.toString;
+		this.storage[this.key] = value;
+		this.key++;
+		this.objectSize++;
+	},
+
+	dequeue: function() {
+		var deqqed = this.storage[this.deq];
+		delete this.storage[this.deq];
+		this.deq++;
+		this.objectSize--;
+		return deqqed;
+	},
+
+	size: function() {
+		if (this.objectSize <= 0) {
+			return 0;
+		}
+
+		return this.objectSize;
+	}
+
+};
 
 
