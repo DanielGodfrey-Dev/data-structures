2bd7af560c924d03f26a3b5870900a80e1d0c3a8
Steven Papalia <stevenapapalia@yahoo.com>
2020-02-06 20:38:33 -0800

    LinkedList passes all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,17 +2,33 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  console.log('hello');
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var newHead = list.head.next;
+    var currentHead = list.head;
+    delete list.head;
+    list.head = newHead;
+    return currentHead.value;
   };
 
   list.contains = function(target) {
+    for (var key in list) {
+      if (list[key].value === target) {
+        return true;
+      }
+    }
+    return false;
   };
-
   return list;
 };
 
