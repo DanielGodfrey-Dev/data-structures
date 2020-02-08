c551b9e95dc277c7f635026f53e28d925b7d9d04
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-02-07 20:37:31 -0800

    Attempted HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,18 +7,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // if no element at index
+//console.log(this._storage);
+  if (!Array.isArray(this._storage.get(index))) { 
+    // create an array and push [index, v] into it
+    this._storage.set(index, [[k, v]]);
+  } else {
+  // else push [index, v] into array
+    var arrAtIndex = this._storage.get(index);
+    arrAtIndex.push([k, v]);
+    this._storage.set(index, arrAtIndex);
+  } 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 };
 
-
-
+var myTable = new HashTable();
+console.log(myTable._storage);
 /*
  * Complexity: What is the time complexity of the above functions?
  */
